43adb601b1f4997c14bc6d91bdda754173d9d2a0
poketecop <jorgeai93b@gmail.com>
2025-01-31 10:46:00 +0100

    Añade ejercicio

---
+++ 4ene20blockMask/f20.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void f1(int sig);
+
+main(int a, char *b[]) {
+
+    long c, d;
+    signal(SIGUSR1, f1);
+    signal(SIGUSR2, SIG_DFL);
+    d = atoi(b[1]);
+    if (a == 2 && d == 1) {
+        c = sigsetmask(sigmask(SIGUSR1));
+        sigblock(sigmask(SIGUSR2));
+        pause();
+        sigsetmask(c);
+    } else {
+        pause();
+        exit(1);
+    }
+}
+
+void f1(int sig)
+{
+    printf("\n\n Texto 1\n", strsignal(sig));
+}
